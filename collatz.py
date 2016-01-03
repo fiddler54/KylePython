c6b83f7d0af655eb909f887a75f2d5749fe0f5da
Kyle <kyle.ziegler54@gmail.com>
2016-01-03 15:24:37 -0700

    Added Collatz program

---
+++ collatz.py
@@ -0,0 +1,27 @@
+#This is the Collatz Sequence.
+#This is commonly known as "the simplest impossible math problem."
+
+def collatz(numb):
+    if numb % 2 == 0:
+        return numb//2
+    else:
+        return numb*3 + 1
+
+def redone(usernumb):
+    while usernumb!=1:
+        display = collatz(usernumb)
+        print(display)
+        usernumb = display
+
+userinput = 0
+print('Welcome To Collatz')
+print('Please type the number on which you wish to perform the Collatz sequence')
+print('Type "exit" to exit')
+while True:
+    userinput = input()
+    if userinput == 'exit' :
+        break
+    if userinput == str(0) :
+        print('Please type a number >= 1')
+        continue
+    redone(int(userinput))
